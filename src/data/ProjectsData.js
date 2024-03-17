2ef12c94a5657c0ca23d25067704739d345c9c45
mohamed.dahani.dev <mohamed.dahani.dev@gmail.com>
2024-03-17 17:58:40 +0100

    Fix The Disc and links of Cards

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,49 +1,53 @@
 import cyborgGamingImg from "../images/cards images/cyborgGaming.png";
 import curdImg from "../images/cards images/crud.png";
-import xoImg from "../images/cards images/xo.png";
 import imageEditorImg from "../images/cards images/imageEditor.png";
 import sunnySideImg from "../images/cards images/sunnySide.png";
-import multiStepFormImg from "../images/cards images/multiStepForm.png";
-import newsHomePageImg from "../images/cards images/newsHomePage.png";
-import newsLetterSignUpImg from "../images/cards images/newsLetterSignUp.png";
-import debitCardImg from "../images/cards images/debitCard.png";
+import crudSystemExJsImg from "../images/cards images/crudSystemExJs.png";
+import eCommerceWithReactImg from "../images/cards images/eCommerceWithReact.png";
+import responsiveDashboardImg from "../images/cards images/responsiveDashboard.png";
+import personalPortfolioImg from "../images/cards images/personalPortfolio.png";
 
 const projectsData = [
   {
     id: 1,
     category: "reactjs",
-    img: cyborgGamingImg,
-    title: "Cyborg Gaming",
+    img: eCommerceWithReactImg,
+    title: "E-Commerce",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "https://github.com/mohamed-dahani-dev/Cyborg-Gaming",
+      "Revolutionize online shopping with our React full-stack e-commerce website, seamlessly integrating frontend and backend technologies for a dynamic, user-friendly experience.",
+    gitHub: "",
+    link: "",
   },
   {
     id: 2,
-    category: "javascript",
-    img: curdImg,
-    title: "CRUD System",
+    category: "reactjs",
+    img: cyborgGamingImg,
+    title: "Cyborg Gaming",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
+      "Dynamic gaming website developed with React, offering immersive user experience, seamless navigation, and interactive gameplay features for passionate gamers worldwide.",
+    gitHub: "https://github.com/mohamed-dahani-dev/Cyborg-Gaming",
+    link: "",
   },
   {
     id: 3,
     category: "javascript",
-    img: xoImg,
-    title: "XO",
+    img: curdImg,
+    title: "CRUD System",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
+      "A CRUD system project using HTML, CSS, and JavaScript allows users to Create, Read, Update, and Delete data. It enables efficient management of information with a user-friendly interface.",
+    gitHub: "https://github.com/mohamed-dahani-dev/CRUDS",
+    link: "https://crud-system-html-css-js.web.app/",
   },
+
   {
     id: 4,
     category: "javascript",
     img: imageEditorImg,
-    title: "Image Editore",
+    title: "Image Editor",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
+      "An innovative image editor project combining HTML, CSS, and JavaScript, offering intuitive user interface and diverse editing functionalities for creative image manipulation and enhancement.",
+    gitHub: "https://github.com/mohamed-dahani-dev/Make-A-Image-Editor",
+    link: "https://image-editor-992e9.web.app",
   },
   {
     id: 5,
@@ -51,44 +55,40 @@ const projectsData = [
     img: sunnySideImg,
     title: "Sunny Side",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
+      "Project Sunnyside website showcases vibrant design and seamless functionality using HTML and CSS. Its engaging layout and dynamic features captivate visitors, fostering an enjoyable browsing experience.",
+    gitHub:
+      "https://github.com/mohamed-dahani-dev/sunnyside-agency-landing-page-main",
+    link: "https://sunnyside-5291e.web.app/",
   },
   {
-    id: 6,
-    category: "javascript",
-    img: multiStepFormImg,
-    title: "Multi Step Form",
+    id: 10,
+    category: "node&express",
+    img: crudSystemExJsImg,
+    title: "Crud System Express Js",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
+      "A CRUD system using Express.js enables Create, Read, Update, and Delete operations on data. It utilizes Node.js and Express for backend functionality, facilitating efficient database interactions for web applications.",
+    gitHub: "https://github.com/mohamed-dahani-dev/CRUD-SYSTEM-EX-JS",
+    link: "https://crud-system-ex-js.onrender.com",
   },
   {
-    id: 7,
+    id: 11,
     category: "html&css",
-    img: newsHomePageImg,
-    title: "News Home Page",
+    img: responsiveDashboardImg,
+    title: "Responsive Dashboard",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
+      "A responsive HTML/CSS project dashboard displays dynamic data visually, providing an intuitive interface for tracking progress, metrics, and insights efficiently across various devices with adaptability and clarity..",
+    gitHub: "https://github.com/mohamed-dahani-dev/Responsive-Dashboard",
+    link: "https://responsive-dashboard-93af4.web.app/",
   },
   {
-    id: 8,
-    category: "html&css",
-    img: newsLetterSignUpImg,
-    title: "News Letter Sign Up",
+    id: 12,
+    category: "reactjs",
+    img: personalPortfolioImg,
+    title: "Personal Portfolio",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
-  },
-  {
-    id: 9,
-    category: "javascript",
-    img: debitCardImg,
-    title: "Debit Card",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorum optio assumenda officiis beatae ducimus, atque perspiciatis nostrum quaerat dolor nisi reiciendis in.",
-    gitHub: "",
+      "Create a dynamic personal portfolio using React, showcasing skills, projects, and experiences with modern design and seamless navigation for optimal user engagement and professional presentation.",
+    gitHub: "https://github.com/mohamed-dahani-dev/my-portfolio-v1",
+    link: "https://my-portfolio-bc2db.web.app/",
   },
 ];
 export default projectsData;
